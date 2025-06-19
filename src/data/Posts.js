98d68b506cd69e7990c7330f4d059c15cdd9456e
Ljove02 <135197334+Ljove02@users.noreply.github.com>
2025-06-19 18:59:19 +0200

    gitIgnore updated

---
+++ src/data/Posts.js
@@ -0,0 +1,193 @@
+// src/data/mockPosts.js
+
+// Privremeni podaci - Idealno bi dolazili iz API-ja ili CMS-a
+export const allPosts = [
+  {
+    id: 1,
+    slug: "startup-kamp-ntp-parkup",
+    description: "Moje Iskustvo na 'NTP ParkUp!' Kampu: Uvid u Svet Startupa",
+    keywords:
+      "NTP ParkUp!, startup kamp, startup, VR, ambliopija, investitori, timski rad, startup vrednosti",
+    title: {
+      sr: "Moje Iskustvo na 'NTP ParkUp!' Kampu: Uvid u Svet Startupa",
+      en: `My Experience at the ‘NTP ParkUp!’ 
+          Camp: A Dive into the World of Startups`,
+    },
+    content: {
+      sr: `
+        <p class="post-content-main_bold">
+        Prvog dana smo jedva razumeli šta znači imati start-up. A poslednjeg, već smo stajali pred pravim investitorima, prezentujući našu ideju o VR uređaju za detekciju ambliopije. A ja sam bio deo tima koji je sve to postigao.
+        </p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img" src="/assets/blog_1/ntp-parkup-1.avif" alt="NTP ParkUp! 1" />
+        </div>
+        <p>
+        Svake godine, Naučno-tehnološki park u Srbiji organizuje kamp pod nazivom “NTP ParkUp!” za studente zainteresovane za svet startupa.
+        Cilj programa je da mladim preduzetnicima pruži znanje i veštine potrebne za pokretanje sopstvenih startupa, kroz intenzivnu obuku i rad sa mentorima.
+        </p>
+        <p>
+        Ove godine, imao sam priliku da učestvujem na kampu, gde sam zajedno sa timom radio na razvoju inovativnog VR uređaja za detekciju ambliopije.
+        Tokom kampa, prošli smo kroz sve faze stvaranja startupa, od ideje do prezentacije pred pravim investitorima.
+        </p>
+        <p>
+        Kroz četiri dana intenzivnog rada, uz podršku iskusnih mentora, naučili smo kako da definišemo problem, istražimo tržište, razvijemo proizvod, i na kraju ga uspešno prezentujemo.
+        Bilo je to nezaboravno iskustvo koje mi je pružilo dubok uvid u proces razvoja startupa.
+        </p>
+        <h2>1) Svi Mogu Naučiti Kako da Pokrenu Start-up!</h2>
+        <p class="post-content-main_bold">Da, baš svako.</p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img_2" src="/assets/blog_1/ntp-parkup-2.avif" alt="NTP ParkUp! 2" />
+        </div>
+        <p>
+        Uvek sam želeo da imam svoj start-up, ali nisam imao jasnu predstavu o tome kako da započnem i šta je sve potrebno za uspešan start-up. 
+        Na kampu sam kroz konkretne primere i mentorske sesije konačno razjasnio mnoge nedoumice koje sam imao.
+        </p>
+        <p>
+        Naučio sam da, iako startapovi mogu delovati zastrašujuće, zapravo se sve svodi na sistematičan pristup. 
+        Kroz rad na našem projektu, shvatio sam koliko je važno postaviti čvrste temelje: od prepoznavanja prave ideje, preko detaljnog istraživanja tržišta, do jasnog definisanja problema koji naš proizvod rešava.
+        </p>
+        <p>
+        Mentori su nam pružili dragocene savete i usmeravali nas kada je to bilo potrebno. 
+        Pomogli su nam da sa više sigurnosti koračamo kroz proces razvoja.
+        </p>
+        <p>
+        Sada imam ne samo jasniju sliku o celokupnom procesu, već i samopouzdanje da krenem napred, znajući tačno koje korake treba da preduzmem da bi moj start-up bio uspešan.
+        </p>
+        <h2>2) Tim, Ideje i Veštine: Temelji Uspešnog Startupa</h2>
+        <p>
+        Jedan od najznačajnijih aspekata ovog kampa bio je rad u timu. 
+        Kroz intenzivne timske aktivnosti, naučio sam koliko je važna komunikacija unutar tima i kako svi članovi moraju biti na istoj strani kada je u pitanju cilj koji želimo da postignemo.
+        </p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img_3" src="/assets/blog_1/ntp-parkup-3.avif" alt="NTP ParkUp! 2" />
+        </div>
+        <p class="description-of-image">Rad sa Mentorom</p>
+        <p>
+        Naučio sam kako da prepoznajem pravu ideju, kako da definišem problem i analiziram tržište, ali i kako da efikasno komuniciram unutar tima. 
+        Kroz svakodnevni rad na našem projektu, sve ove veštine su bile testirane u praksi, što mi je omogućilo da steknem neprocenjivo iskustvo.
+        </p>
+        <p>
+        Posebno zanimljivo iskustvo bio je treći dan kada smo vežbali prezentacione veštine kroz glumu, što nam je pomoglo da se oslobodimo treme i pripremimo za finalni pitch pred investitorima.
+        </p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img_4" src="/assets/blog_1/ntp-parkup-4.avif" alt="NTP ParkUp! 2" />
+        </div>
+        <p class="description-of-image">RolePlay Aktivnost</p>
+        <p>
+        Ova vežba nam je dala priliku da na zabavan i interaktivan način prevaziđemo strah od javnog nastupa, čime smo se još bolje pripremili za stvarne izazove koji su nas očekivali.
+        </p>
+        <h2>3) Pitching Pred Investitorima: Od Straha do Uspeha</h2>
+        <p>
+        Pitching pred pravim investitorima bio je emocionalni rolerkoster. 
+        Tokom probnih prezentacija, savladavao sam tremu i učio kako da bolje prenesem svoju ideju. 
+        Prvi pokušaji su bili izazovni, ali uz pomoć mentora uspeli smo da poboljšamo naše prezentacije.
+        </p>
+        <p>
+        Na finalnom pitchu, jedan od investitora izrazio je zabrinutost zbog mogućih nuspojava korišćenja VR uređaja, koje su najčešće zabeležene u gaming industriji. 
+        Shvatio sam da je njegova perspektiva zasnovana na statistici koja uključuje korisnike u gaming-u, gde se VR uređaji koriste duže nego što je preporučeno.
+        </p>
+        <p>
+        Zbog tog ekstremnog korišćenja, statistika nuspojava je iskrivljena i stvara pogrešnu sliku o VR tehnologiji. 
+        Objasnio sam da naš uređaj, namenjen za medicinsku primenu, neće biti korišćen na taj način, već u kontrolisanim i kratkim vremenskim intervalima, čime se smanjuje rizik od nuspojava.
+        </p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img_5" src="/assets/blog_1/ntp-parkup-5.avif" alt="NTP ParkUp! 2" />
+        </div>
+        <p class="description-of-image">Ja Pitch-ujem investitorima :)</p>
+        <p>
+        Bilo je izuzetno zadovoljstvo kada smo osvojili treće mesto, a posebno sam ponosan na svoj tim i način na koji smo zajedno savladali izazove.
+        </p>
+        <h2>4) Lekcije o Timskim Vrednostima</h2>
+        <p>
+        Jedan od najvažnijih momenata na kampu dogodio se na kraju jednog predavanja, kada nam je dodeljen zadatak da u timovima radimo na očuvanju angažovanosti i definisanju vrednosti tima. 
+        Dok smo svi bili uzbuđeni zbog zabavnih delova zadatka, poput trčanja po zgradi u potrazi za predmetima, većina nas nije pridavala veliku pažnju delu koji se odnosio na timske vrednosti.
+        </p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img_6" src="/assets/blog_1/ntp-parkup-6.avif" alt="NTP ParkUp! 2" />
+        </div>
+        <p>
+        Kada je došlo vreme za prezentaciju, svi timovi su pokazali svoje rezultate. 
+        Na naše iznenađenje, tim koji je pobedio nije sakupio sve predmete sa liste, ali su briljirali u objašnjavanju vrednosti koje dele i zašto su im te vrednosti bitne.
+        </p>
+        <p>
+        Mentor je tada otkrio da je upravo taj deo zadatka nosio najveću težinu u ocenjivanju, čak više od svih drugih delova zajedno. 
+        Bilo je to veliko otkrovenje za sve nas – lekcija da su vrednosti i zajedništvo tima daleko važniji od bilo koje pojedinačne aktivnosti.
+        </p>
+        <h2>5) Zakljucak</h2>
+        <p>
+        Ovaj kamp mi je otvorio oči i pokazao koliko su tim i ljudi sa kojima radiš najvažniji aspekt svakog startupa. 
+        Shvatio sam da, bez obzira na ideju, uspeh zavisi od posvećenosti, timskog rada i podrške članova tima.
+        </p>
+        <p>
+        Upoznao sam sjajne prijatelje i stekao nova znanja koja ću nositi sa sobom u buduće projekte. 
+        Na kraju dana, naš trud i rad su se isplatili, a treće mesto koje smo osvojili za mene ima ogromnu vrednost.
+        </p>
+        <p>
+        Ipak, ono što je taj trenutak učinilo posebnim jeste to što se sve desilo baš na moj rođendan, što je pobedi dalo dodatni značaj. 
+        Osvajanje trećeg mesta bilo je nezaboravno iskustvo i savršen način da proslavim sa timom koji je postao više od saradnika—postali su pravi prijatelji.
+        </p>
+      `,
+      en: `
+        <p class="post-content-main_bold">On the first day, we barely understood what it meant to have a start-up. But by the last day, we were standing in front of real investors, pitching our idea for a VR device to detect amblyopia. And I was part of the team that made it all happen.</p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img" src="/assets/blog_1/ntp-parkup-1.avif" alt="NTP ParkUp! 1" />
+        </div>
+        <p>Each year, the Science and Technology Park in Serbia organises a camp called “NTP ParkUp!” for students interested in the world of startups. The program aims to equip young entrepreneurs with the knowledge and skills needed to launch their own startups through intensive training and mentorship.</p>
+        <p>This year, I had the opportunity to participate in the camp, where I worked with a team on developing an innovative VR device for detecting amblyopia. Throughout the camp, we went through all the stages of building a startup, from ideation to pitching to real investors.</p>
+        <p>Over four days of intense work, supported by experienced mentors, we learned how to define problems, research the market, develop a product, and successfully present it. It was an unforgettable experience that gave me deep insight into the startup development process.</p>
+        <h2>1) Everyone Can Learn How to Start a Startup!</h2>
+        <p class="post-content-main_bold">Yes, everyone.</p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img_2" src="/assets/blog_1/ntp-parkup-2.avif" alt="NTP ParkUp! 2" />
+        </div>
+        <p>I’ve always wanted to have my own startup, but I never had a clear idea of how to begin or what was needed for a successful startup. At the camp, through practical examples and mentoring sessions, I finally clarified many misconceptions I had.</p>
+        <p>I learned that, although startups can seem daunting, it all boils down to a systematic approach. Working on our project, I realised how important it is to lay a strong foundation: from identifying the right idea, thoroughly researching the market, to clearly defining the problem our product solves.</p>
+        <p>The mentors provided valuable insights and guided us when necessary. They helped us navigate the development process with more confidence.</p>
+        <p>Now, I not only have a clearer picture of the entire process but also the confidence to move forward, knowing exactly which steps to take to make my startup successful.</p>
+        <h2>2) Teamwork, Ideas, and Skills: The Foundations of a Successful Startup</h2>
+        <p>One of the most significant aspects of this camp was working in a team. Through intense team activities, I learned how vital communication within the team is and how everyone needs to be aligned when it comes to the goal we want to achieve.</p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img_3" src="/assets/blog_1/ntp-parkup-3.avif" alt="NTP ParkUp! 2" />
+        </div>
+        <p class="description-of-image">Working Together with Our Mentor</p>
+        <p>I learned how to identify the right idea, define the problem, and analyse the market, but also how to communicate effectively within the team. Through daily work on our project, all these skills were tested in practice, allowing me to gain invaluable experience.</p>
+        <p>A particularly interesting experience was on the third day when we practiced presentation skills through role-play, which helped us overcome stage fright and prepare for the final pitch to investors.</p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img_4" src="/assets/blog_1/ntp-parkup-4.avif" alt="NTP ParkUp! 2" />
+        </div>
+        <p class="description-of-image">RolePlay Activity</p>
+        <p>This exercise gave us the opportunity to overcome the fear of public speaking in a fun and interactive way, making us even better prepared for the real challenges that awaited us.</p>
+        <h2>3) Pitching to Investors: From Fear to Success</h2>
+        <p>Pitching to real investors was an emotional rollercoaster. During the trial presentations, I overcame my nerves and learned how to better convey my idea. The initial attempts were challenging, but our mentors provided valuable advice that helped us improve our presentations.</p>
+        <p>During the final pitch, one of the investors expressed concern about potential side effects of using VR devices, commonly seen in the gaming industry. I realised his perspective was based on statistics from gaming, where VR devices are used for longer periods than recommended.</p>
+        <p>Due to this excessive use, the statistics on side effects are skewed, creating a misleading picture of VR technology. I explained that our device, intended for medical use, wouldn’t be used in the same way but rather in controlled, short intervals, minimising the risk of side effects.</p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img_5" src="/assets/blog_1/ntp-parkup-5.avif" alt="NTP ParkUp! 2" />
+        </div>
+        <p class="description-of-image">Me Pitching to Investors :)</p>
+        <p>It was incredibly satisfying when we secured third place, and I’m especially proud of my team and how we tackled the challenges together.</p>
+        <h2>4) Lessons in Team Values</h2>
+        <p>One of the most important moments at the camp happened at the end of a lecture when we were given a task to work in teams on maintaining engagement and defining team values. While we were all excited about the fun parts of the task, like running around the building searching for items, most of us didn’t pay much attention to the part about team values.</p>
+        <div class="post-content-main-image">
+          <img class="post-content-main_image_img_6" src="/assets/blog_1/ntp-parkup-6.avif" alt="NTP ParkUp! 2" />
+        </div>
+        <p>When it came time to present, all the teams showed their results. To our surprise, the winning team hadn’t collected all the items from the list, but they excelled in explaining the values they shared and why those values mattered.</p>
+        <p>The mentor then revealed that this part of the task carried the most weight in the evaluation, even more than all the other parts combined. It was a significant revelation for all of us—a lesson that the values and unity of the team are far more important than any individual activity.</p>
+        <h2>5) Conclusion</h2>
+        <p>This camp opened my eyes to how crucial the team and the people you work with are in any startup. I realised that, regardless of the idea, success depends on dedication, teamwork, and the support of team members.</p>
+        <p>I met amazing friends and gained new knowledge that I will carry with me into future projects. At the end of the day, our hard work paid off, and winning third place holds immense value for me.</p>
+        <p>However, what made that moment truly special was that it all happened on my birthday, adding extra significance to the victory. Winning third place was an unforgettable experience and the perfect way to celebrate with a team that became more than just colleagues—they became true friends.</p>
+      `,
+    },
+    category: "blog",
+    date: "Aug, 27, 2024",
+    readTime: {
+      sr: "~6 minuta",
+      en: "~6 minutes",
+    },
+    audioSr: "/assets/blog_1/voice_over/ntpParkUp_sr.MP3", // Putanja iz public foldera
+    audioEn: "/assets/blog_1/voice_over/ntpParkUp_en.MP3",
+  },
+
+  // Dodajte još postova po potrebi
+];
